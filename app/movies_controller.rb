d30da43172a66df897fa25be1eaa5d07d03615f2
Fiona Lapham <fionadlapham@gmail.com>
2018-06-04 09:52:24 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -60,7 +60,12 @@ end
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by
   # release date descending
-  __
+  movies = Movie.all.map do |movie|
+    movie.release_date > 2002 
+    movie.release_date
+    binding.pry
+  end
+  movies  
 end
 
 def can_be_found_updated_and_saved

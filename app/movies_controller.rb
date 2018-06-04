7e8483b12aeeea08c00b7f48dee790cfaff5577c
Fiona Lapham <fionadlapham@gmail.com>
2018-06-04 10:07:53 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -65,10 +65,10 @@ end
 
 def can_be_found_updated_and_saved
   # Updtate the title "Awesome Flick" to "Even Awesomer Flick"
-  Movie.create(title: "Awesome Flick")
-  __
-  __
-  __
+  movie = Movie.create(title: "Awesome Flick")
+  movie1= Movie.find_by(title: "Awesome Flick")
+  movie2 = movie1.update(tite: "Even Awesomer Flick")
+  movie2.save
 end
 
 def can_update_using_update_method
